7f13fddcaa7d0baf3485db9bb6ab14cbf996af3e
David Parrish <david@dparrish.com>
2010-03-01 18:04:50 +1100

    Update all code to follow a consistent coding style.

--- RolloutServer.pm
+++ RolloutServer.pm
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+# vim:tw=100 sw=2 expandtab ft=perl
 
 package RolloutServer;
 
@@ -13,67 +14,61 @@ use URI::Escape;
 use base qw( HTTP::Server::Simple::CGI );
 
 sub new {
-	my($class, $listen, $allow, $base) = @_;
-	my $self = bless(new HTTP::Server::Simple::CGI, $class);
+  my($class, $listen, $allow, $base) = @_;
+  my $self = bless(new HTTP::Server::Simple::CGI, $class);
 
-	$self->{base} = $base;
+  $self->{base} = $base;
 
-	$self->{allow} = [];
-	foreach (split /,/, $allow)
-	{
-		my $net = new Net::Netmask $_;
-		push @{$self->{allow}}, $net;
-	}
+  $self->{allow} = [];
+  foreach (split /,/, $allow) {
+    my $net = new Net::Netmask $_;
+    push @{$self->{allow}}, $net;
+  }
 
-	my($address, $port) = split /:/, $listen;
-	$self->host($address);
-	$self->port($port);
+  my($address, $port) = split /:/, $listen;
+  $self->host($address);
+  $self->port($port);
 
-	return $self;
+  return $self;
 }
 
 
 sub handle_request {
-	my($self, $cgi) = @_;
-	my $path = canonpath(uri_unescape($cgi->path_info()));
+  my($self, $cgi) = @_;
+  my $path = canonpath(uri_unescape($cgi->path_info()));
 
-	printf STDERR "%s %s %s [%s] \"%s %s %s\" %s %s\n",
-		$cgi->remote_host(), "-", "-", scalar(localtime()), $cgi->request_method(),
-		$path, "HTTP/1.0", "200", 0;
+  printf STDERR "%s %s %s [%s] \"%s %s %s\" %s %s\n",
+    $cgi->remote_host(), "-", "-", scalar(localtime()), $cgi->request_method(),
+    $path, "HTTP/1.0", "200", 0;
 
-	my $found = 0;
-	foreach (@{$self->{allow}})
-	{
-		$found++ if $_->match($cgi->remote_host());
-	}
-	if (!$found || $path eq '/')
-	{
-		print "HTTP/1.0 403 Not Allowed\n\n";
-		print "Not allowed to access $path\n";
-	}
+  my $found = 0;
+  foreach (@{$self->{allow}}) {
+    $found++ if $_->match($cgi->remote_host());
+  }
+  if (!$found || $path eq '/') {
+    print "HTTP/1.0 403 Not Allowed\n\n";
+    print "Not allowed to access $path\n";
+  }
 
-	my $localpath = $self->{base}. canonpath(uri_unescape($path));
-	if (-d $localpath)
-	{
-		print "HTTP/1.0 200 OK\n";
-		print "Content-Type: text/html\n\n";
-		print "<html><body><h1>Directory Listing of $path</h1>\n";
-		my $dir = new IO::Dir $localpath;
-		foreach (sort $dir->read())
-		{
-			next if /^\./;
-			$_ .= "/" if -d "$localpath/$_";
-			print "<img src=\"file.jpg\" alt=\"file\"><a href=\"$_\">$_</a> <br>\n";
-		}
-		print "</body></html>\n";
-		return;
-	}
+  my $localpath = $self->{base}. canonpath(uri_unescape($path));
+  if (-d $localpath) {
+    print "HTTP/1.0 200 OK\n";
+    print "Content-Type: text/html\n\n";
+    print "<html><body><h1>Directory Listing of $path</h1>\n";
+    my $dir = new IO::Dir $localpath;
+    foreach (sort $dir->read()) {
+      next if /^\./;
+      $_ .= "/" if -d "$localpath/$_";
+      print "<img src=\"file.jpg\" alt=\"file\"><a href=\"$_\">$_</a> <br>\n";
+    }
+    print "</body></html>\n";
+    return;
+  }
 
-	if (!$self->serve_static($cgi, $self->{base}))
-	{
-		print "HTTP/1.0 404 Not Found\n\n";
-		print "The file you requested was not found\n";
-	}
+  if (!$self->serve_static($cgi, $self->{base})) {
+    print "HTTP/1.0 404 Not Found\n\n";
+    print "The file you requested was not found\n";
+  }
 }
 
 1;
